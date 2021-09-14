6fd26ab00dabcfaffddc80403468ae91776103a2
bst0705 <xiangtaishiqiao0@gmail.com>
2021-09-14 06:45:59 +0000

    3-9

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,54 @@
+class BooksController < ApplicationController
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @books = Book.all
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
+
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    if @book.save
+     flash[:notice] = "Book was successfully updated."
+     redirect_to book_path(@book.id) and return
+    else
+     render :edit
+    end
+
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
+end
